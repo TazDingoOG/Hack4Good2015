b1bd166dfb8051c73be26c3b93c9f0171e047711
TeNNoX <manuel.tennox@gmail.com>
2015-12-05 20:11:13 +0100

    Read accommodactions from DB
    + remove COMMIT stuff from SQL files

---
+++ example_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO `Request` (id,accommodation_id,item_id,expiration) VALUES (1,1,1,NULL),
+ (2,1,2,NULL);
+INSERT INTO `Item` (id,name,image) VALUES (1,'T-Shirt
+',NULL),
+ (2,'Pullover',NULL),
+ (3,'Jacke',NULL);
+INSERT INTO `Accommodation` (id,name,email,telnr,authtoken) VALUES (1,'Test 1','nonofyour@business.de','0123/456789',NULL);
