e30d35ba4278f356004acb09c1f957d5106bbbe4
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2015-12-06 00:11:39 +0100

    changes layout of unterkunft

--- static/js/live-search.js
+++ static/js/live-search.js
@@ -1,6 +1,25 @@
 $("#search").on("keyup", function() {
     var value = $(this).val().toLowerCase();
-    $("table tr").each(function(index) {
+    $("#table1 tr").each(function(index) {
+        if (index !== 0) {
+
+            $row = $(this);
+
+            var id = $row.find("td").eq(1).text().toLowerCase();
+
+            if (id.indexOf(value) >= 0) {
+                $row.show();
+            }else {
+                $row.hide(400);
+            }
+        }
+    });
+});
+
+
+$("#search_modal").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#table_modal tr").each(function(index) {
         if (index !== 0) {
 
             $row = $(this);
