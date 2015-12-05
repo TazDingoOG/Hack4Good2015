c4d1fe598e5de9751c4913efc29ee0057fd495fa
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2015-12-05 18:15:49 +0100

    optimized live search

--- static/js/live-search.js
+++ static/js/live-search.js
@@ -1,17 +1,17 @@
 $("#search").on("keyup", function() {
-    var value = $(this).val();
+    var value = $(this).val().toLowerCase();
     console.log(value);
     $("table tr").each(function(index) {
         if (index !== 0) {
 
             $row = $(this);
 
-            var id = $row.find("td").eq(1).text();
+            var id = $row.find("td").eq(1).text().toLowerCase();
 
-            if (id.indexOf(value) !== 0) {
-                $row.hide(400);
-            }else {
+            if (id.indexOf(value) >= 0) {
                 $row.show();
+            }else {
+                $row.hide(400);
             }
         }
     });
