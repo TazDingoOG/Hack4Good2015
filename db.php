f46fe5d1ef6608a2e651fbe47983b65013974b86
TeNNoX <manuel.tennox@gmail.com>
2015-12-05 19:12:43 +0100

    Database has to exist, else die

--- db.php
+++ db.php
@@ -3,13 +3,9 @@
 class MyDB extends SQLite3 {
 
     const DB_FILENAME = 'data.db'; // contains the database (sqlite3)
-    const DB_DEFAULT_FILENAME = 'data_default.db'; // contains the database (sqlite3)
-
+    
     function __construct()
     {
-        if(!is_file(MyDB::DB_FILENAME)) {
-            copy(MyDB::DB_DEFAULT_FILENAME, MyDB::DB_FILENAME) or die("Failed to copy default DB");
-        }
         $this->open(MyDB::DB_FILENAME);
     }
 }
