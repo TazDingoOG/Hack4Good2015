68be159fa264bde1f0112bae808f6002647c6d1a
TeNNoX <manuel.tennox@gmail.com>
2015-12-05 16:23:29 +0100

    Move frontend statics to static/
    + small fix in index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 
 // initiate Loader (loads the HTML stuff from the directory 'templates')
-$loader = new Twig_Loader_Filesystem(__DIR__ . 'templates/');
+$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates/');
 $twig = new Twig_Environment($loader, array(
     /* <!--'cache' => '/path/to/compilation_cache', */
 ));
