472cfe5cd46e47c3484caa637afefc7f175aea00
TeNNoX <manuel.tennox@gmail.com>
2015-12-05 21:58:36 +0100

    [unfinished] nice urls

--- index.php
+++ index.php
@@ -1,18 +1,76 @@
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php'; // composer autoloader
 
-require_once('db.php');
+class Volunteerio
+{
+    public $twig;
+    public $db;
 
-// DATABASE //
-$db = new MyDB();
+    function __construct()
+    {
+        // DATABASE //
+        require_once('db.php');
+        $this->db = new MyDB();
 
-$requests = $db->getRequestsForAccommodation(1);
+        // TEMPLATE ENGINE //
+        $loader = new Twig_Loader_Filesystem(__DIR__ . '/templates/'); // initiate Loader (loads the HTML stuff from the directory 'templates')
+        $this->twig = new Twig_Environment($loader, array(
+            /* <!--'cache' => '/path/to/compilation_cache', */      //TODO: enable caching later
+        ));
+    }
 
-// TEMPLATE ENGINE //
-$loader = new Twig_Loader_Filesystem(__DIR__ . '/templates/'); // initiate Loader (loads the HTML stuff from the directory 'templates')
-$twig = new Twig_Environment($loader, array(
-    /* <!--'cache' => '/path/to/compilation_cache', */      //TODO: enable caching later
-));
+    /** serve the site for that specific url */
+    function serve_url($url)
+    {
+        // parse URL
+        $path = parse_url($url, PHP_URL_PATH);
 
-echo $twig->render('index.html', array('requests' => $requests));
+        // -> /unterkunft/NAME-DER-UNTERKUNFT
+        if ($path == "/") {
+            $this->render_homepage();
+        } else if (strpos($path, '/unterkunft') === 0) {
+            $acom = substr($path, 12); // remove the '/unterkunft/'
+
+            $this->render_accommodation($acom);
+        } else if (strpos($path, '/a') === 0) {
+            $token = substr($path, 3); // remove the '/a/'
+
+            $acom = $this->handle_token($token);
+
+            $this->render_accommodation($acom);
+        } else {
+            $this->render_404();
+        }
+    }
+
+    function render_homepage()
+    {
+        echo $this->twig->render('index.html');
+        exit;
+    }
+
+    function render_accommodation($accommodation)
+    {
+        $requests = $this->db->getRequestsForAccommodation(1);
+
+        echo "TODO";
+        exit;
+    }
+
+    function handle_token($token)
+    {
+        echo "TODO";
+        return "";
+    }
+
+    function render_404()
+    {
+        echo $this->twig->render('404.html');
+        exit;
+    }
+}
+
+$volunteerio = new Volunteerio();
+$volunteerio->serve_url($_SERVER['REQUEST_URI']);
+
 ?>
