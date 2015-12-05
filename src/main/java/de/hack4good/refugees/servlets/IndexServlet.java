684ff1421b213cec8af31a14bdc3af83997293a8
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2015-12-05 14:35:33 +0100

    better hello world

--- src/main/java/de/hack4good/refugees/servlets/IndexServlet.java
+++ src/main/java/de/hack4good/refugees/servlets/IndexServlet.java
@@ -25,7 +25,8 @@ public class IndexServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.setAttribute("text", "Hello World");
+		request.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 
 	/**
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,5 @@
 <html>
 <body>
-<h2>Hello World!</h2>
+<h2><%=request.getAttribute("text") %></h2>
 </body>
 </html>
