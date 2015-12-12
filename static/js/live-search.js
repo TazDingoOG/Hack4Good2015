744b5f3f3cb884ac2dfaca532067e9f9d574bbcb
TeNNoX <manuel.tennox@gmail.com>
2015-12-12 02:16:30 +0100

    Show bootstrap error when ajax saving fails
    - add id 'main_container' to main container

--- static/js/live-search.js
+++ static/js/live-search.js
@@ -14,7 +14,7 @@ $("#search").on("keyup", function () {
             $row.hide(400);
         }
     });
-    if(!bol) {
+    if (!bol) {
         $("#table_content").hide(400);
     } else {
         $("#table_content").show(400);
@@ -33,16 +33,16 @@ $("#modal_search").on("keyup", function () {
 
         if (id.indexOf(value) >= 0) {
             row.show();
-            if(id == value) {
-               bol = true;
+            if (id == value) {
+                bol = true;
             }
         } else {
             row.hide(200);
         }
     });
-    if(value.length > 0){
-        if(!bol){
-           $("#table-new-elem").show();
+    if (value.length > 0) {
+        if (!bol) {
+            $("#table-new-elem").show();
             $("#item-row-content").html(org_value);
         } else {
             $("#table-new-elem").hide();
@@ -54,18 +54,34 @@ $("#modal_search").on("keyup", function () {
 
 
 /** animate btn hovers **/
-$(document.body).on('mouseenter', "[data-hoverclass]", function() {
+$(document.body).on('mouseenter', "[data-hoverclass]", function () {
     $(this).addClass($(this).data('hoverclass'));
 });
-$(document.body).on('mouseleave', "[data-hoverclass]", function() {
+$(document.body).on('mouseleave', "[data-hoverclass]", function () {
     $(this).removeClass($(this).data('hoverclass'));
 });
 
+/** shows a bootstrap alert at the top of #main_container
+ *
+ * @param type - danger|info|warning|success
+ * @param strong - Something like 'Error:' to display bold before the actual message
+ * @param msg - the actual message
+ */
+function show_alert(type, strong, msg) {
+    var alert = $('<div class="alert alert-' + type + '" style="display: none;">'
+        + '<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
+        + (strong ? '<strong>' + strong + '</strong> ' : '')
+        + msg
+        + '</div>');
+    alert.prependTo("#main_container").fadeIn('slow', function () { // manually fade in, and add the fade classes after
+        $(this).addClass("fade in");                                // fading finished (they are needed for fading out)
+    });                                                             // - I did not get it to work in a better way :/
+}
 
 function fail(err) {
-    //alert(err);
+    show_alert('danger', "Error", "Could not save changes to server (see console for more info)");
     console.log(err);
-    window.location.reload(); // for now, just reload (we dont have sync)
+    //window.location.reload(); // for now, just reload (we dont have sync)
 }
 
 /** Remove items via 'x' **/
