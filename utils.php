0a5eb053064a6ef17defce10792d29dacdbcab0e
TeNNoX <manuel.tennox@gmail.com>
2015-12-15 01:30:43 +0100

    Auto detect item images
    If no image file is given in the DB, look for a file like 'clean_name_of_item.svg'.
    
    - remove old images (that were not quite legal, i suppose - placeholders)
    - rename new images to german names (for now, we don't have translation...)
    - the image_url is now generated in Utils::generateIconUrls(...)
    
    + add todo to registration_post_handle
    
    yep... I was bored...

--- utils.php
+++ utils.php
@@ -6,14 +6,40 @@ class Utils
     public static function generateCleanName($name)
     {
         $clean = strtolower($name);
-        $clean = str_replace('ä', 'ae', $clean);
-        $clean = str_replace('ö', 'oe', $clean);
-        $clean = str_replace('ü', 'ue', $clean);
-        $clean = str_replace('ß', 'ss', $clean);
 
-        $clean = preg_replace('/[\w]+/', "-", $clean); // replace whitespace
+        // TODO: umlaut replacement doesn't work :/
+        /*$clean = str_replace("ä", "ae", $clean);
+        $clean = str_replace("ö", "oe", $clean);
+        $clean = str_replace("ü", "ue", $clean);
+        $clean = str_replace("ß", "ss", $clean);*/
+
+        $clean = preg_replace('/[\s.]+/', "-", $clean); // replace whitespace
         $clean = preg_replace('/[^a-z0-9-]+/', '', $clean); // remove filthy characters
+
         return $clean;
+    }
+
+    /**
+     * Try to find missing icon files, and place them in the array
+     *
+     * @param array $requests - array of requests
+     * @param string $icon_dir - directory where the icons are located, with trailing slash
+     * @param string $icon_base_url - url to prepend for the embedded images
+     */
+    public static function generateIconUrls(&$requests, $icon_dir, $icon_base_url)
+    {
+        foreach ($requests as &$req) {
+            if (!empty($req['image'])) {
+                $req['image_url'] = $icon_base_url . $req['image'];
+            } else {
+                $cleanname = self::generateCleanName($req['name']);
+
+                $imgfile = $cleanname . '.svg';
+                if (is_file($icon_dir . $imgfile)) {
+                    $req['image_url'] = $icon_base_url . $imgfile;
+                }
+            }
+        }
     }
 }
 
